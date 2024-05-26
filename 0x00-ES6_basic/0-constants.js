2f91e294435a32633ebc7daa857ef9b219956349
Ifeoluwa oyekanmi <IfeoluwaOyekanmi@users.noreply.github.com>
2024-05-26 11:47:40 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,15 @@
-function taskFirst() {
-    const task = 'Task 1';
-    return task;
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-function taskNext() {
-    let task = 'Task 2';
-    return task;
-} 
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
